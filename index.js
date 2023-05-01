2012ffda356eb0bc1dd5987fca791a8b07c18695
gsparapassi <gsparapassi@wheretech.it>
2023-05-01 18:04:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const b4 = "20vw";
+
+gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin);
+gsap.defaults({ ease: "none" });
+const main = gsap.timeline({
+    defaults: { duration: 0.1 },
+    scrollTrigger: {
+        trigger: "#svg",
+        scrub: true,
+        start: "top center",
+    }
+})
+    .from(".theLine", {
+        drawSVG: 0, onComplete: () => {
+
+            main.to("#rainbow", {
+                duration: 1,
+                width: b4
+            })
+
+        }
+    }, 0)
+    .to("#prisma", { duration: 1, autoAlpha: 1 })
